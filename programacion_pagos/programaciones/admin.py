ed7c524fa2d6d1bf9f306ddb1d8f216d4ba46ef2
danielvillamorales <danielvillamorales@gmail.com>
2025-06-18 09:40:42 -0500

    configurando el admin

--- programacion_pagos/programaciones/admin.py
+++ programacion_pagos/programaciones/admin.py
@@ -1,12 +1,54 @@
 from django.contrib import admin
-from .models import CuentasBancarias, Pagos
+from .models import CuentasBancarias, Pagos, Acuerdos
 
 # Register your models here.
 admin.site.register(CuentasBancarias)
 
 
 @admin.register(Pagos)
-class PermisosAdmin(admin.ModelAdmin):
-    list_display = ('fecha_pago','nit', 'proveedor','valor','estado','empresa','descripcion')
-    search_fields = ('fecha_pago','nit', 'proveedor','valor','estado','empresa','descripcion')
-    ordering = ('-id',)
+class PagosAdmin(admin.ModelAdmin):
+    list_display = (
+        "fecha_pago",
+        "nit",
+        "proveedor",
+        "valor",
+        "estado",
+        "descripcion",
+    )
+    search_fields = (
+        "fecha_pago",
+        "nit",
+        "proveedor",
+        "valor",
+        "estado",
+        "empresa",
+        "descripcion",
+    )
+    list_filter = ("estado", "empresa", "fecha_pago")
+    list_editable = ("estado",)
+    ordering = ("-id",)
+
+
+@admin.register(Acuerdos)
+class AcuerdosAdmin(admin.ModelAdmin):
+    list_display = (
+        "año",
+        "mes",
+        "dia",
+        "nit",
+        "proovedoor",
+        "cuota",
+        "estado",
+    )
+    search_fields = (
+        "año",
+        "mes",
+        "dia",
+        "nit",
+        "proovedoor",
+        "cuota",
+        "estado",
+    )
+    list_filter = ("estado", "año", "mes", "dia")
+    list_editable = ("estado",)
+    ordering = ("-id",)
